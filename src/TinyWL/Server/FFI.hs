5c6184609b2d325f5aff67004d5ba558c5d0468d
Dylan Hachmann <dylanhachmann@gmail.com>
2024-11-22 08:19:31 -0700

    Move handle_keybinding to new file

--- src/TinyWL/Server/FFI.hs
+++ src/TinyWL/Server/FFI.hs
@@ -20,20 +20,3 @@ foreign import ccall "server_set_startup_command" c_server_set_startup_command :
 
 withCArgs :: [String] -> IO (Ptr (Ptr CChar))
 withCArgs args = newArray =<< mapM newCString args
-
-{-
-   * Here we handle compositor keybindings. This is when the compositor is
-   * processing keys, rather than passing them on to the client for its own
-   * processing.
-   *
-   * This function assumes Alt is held down.
--}
-foreign import capi "xkbcommon/xkbcommon-keysyms.h value XKB_KEY_Escape" xK_Escape :: CUInt
-foreign import capi "xkbcommon/xkbcommon-keysyms.h value XKB_KEY_F1" xK_F1 :: CUInt
-foreign import ccall "hs_terminate_display" terminateDisplay :: Ptr TinyWLServer -> CBool
-foreign import ccall "hs_cycle_window_next" cycleWindowNext :: Ptr TinyWLServer -> CBool
-foreign export ccall "handle_keybinding" handleKeybinding :: Ptr TinyWLServer -> CUInt -> CBool
-handleKeybinding server key = case key of
-                                xK_Escape -> terminateDisplay server
-                                xK_F1 -> cycleWindowNext server
-                                _ -> 0 :: CBool
---
+++ tinywl/TinyWL.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE CApiFFI #-}
+
+module TinyWL where
+import Foreign.Ptr
+import Foreign.C.Types
+
+data TinyWLServer
+
+-- foreign import capi "xkbcommon/xkbcommon-keysyms.h value XKB_KEY_Escape" xK_Escape :: CUInt
+-- foreign import capi "xkbcommon/xkbcommon-keysyms.h value XKB_KEY_F1" xK_F1 :: CUInt
+foreign import ccall "hs_terminate_display" terminateDisplay :: Ptr TinyWLServer -> CBool
+foreign import ccall "hs_cycle_window_next" cycleWindowNext :: Ptr TinyWLServer -> CBool
+
+
+{-
+   * Here we handle compositor keybindings. This is when the compositor is
+   * processing keys, rather than passing them on to the client for its own
+   * processing.
+   *
+   * This function assumes Alt is held down.
+-}
+{-
+FIXME: foreign import doesn't seem to be working at the moment; pass
+in the values directly for now
+-}
+foreign export ccall "handle_keybinding" handleKeybinding :: Ptr TinyWLServer -> CUInt -> CBool
+handleKeybinding server key = case key of
+                                0xff1b -> terminateDisplay server
+                                0xffbe -> cycleWindowNext server
+                                _ -> 0 :: CBool
