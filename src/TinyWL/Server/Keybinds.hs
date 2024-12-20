6a9a346e61850c953d7472697187a06dc272b905
Dylan Hachmann <dylanhachmann@gmail.com>
2024-12-19 21:27:10 -0700

    Move keyconfig into assoc list
    Getting closer to typical configuration, now.
    
    Also lift C function returns into IO and use type synonyms for
    convenience

--- src/TinyWL/Server/Keybinds.hs
+++ src/TinyWL/Server/Keybinds.hs
@@ -4,13 +4,18 @@
 module TinyWL.Server.Keybinds where
 import Foreign.Ptr
 import Foreign.C.Types
+import Data.Bits ((.|.))
 
 data TinyWLServer
 
-foreign import capi "xkbcommon/xkbcommon-keysyms.h value XKB_KEY_Escape" xK_Escape :: CUInt
-foreign import capi "xkbcommon/xkbcommon-keysyms.h value XKB_KEY_F1" xK_F1 :: CUInt
-foreign import ccall "hs_terminate_display" terminateDisplay :: Ptr TinyWLServer -> CBool
-foreign import ccall "hs_cycle_window_next" cycleWindowNext :: Ptr TinyWLServer -> CBool
+type Key = CUInt
+type Server = Ptr TinyWLServer
+type Action = Server -> IO CBool
+
+foreign import capi "xkbcommon/xkbcommon-keysyms.h value XKB_KEY_Escape" xK_Escape :: Key
+foreign import capi "xkbcommon/xkbcommon-keysyms.h value XKB_KEY_F1" xK_F1 :: Key
+foreign import ccall "hs_terminate_display" terminateDisplay :: Action
+foreign import ccall "hs_cycle_window_next" cycleWindowNext :: Action
 
 
 {-
@@ -20,9 +25,18 @@ foreign import ccall "hs_cycle_window_next" cycleWindowNext :: Ptr TinyWLServer 
    *
    * This function assumes Alt is held down.
 -}
-handleKeybinding :: Ptr TinyWLServer -> CUInt -> CBool
-foreign export ccall "handle_keybinding" handleKeybinding :: Ptr TinyWLServer -> CUInt -> CBool
-handleKeybinding server key
-  | key == xK_Escape = terminateDisplay server
-  | key == xK_F1 = cycleWindowNext server
-  | otherwise = 0
+handleKeybinding :: Server -> Key -> IO CBool
+foreign export ccall "handle_keybinding" handleKeybinding :: Ptr TinyWLServer -> Key -> IO CBool
+handleKeybinding server key = foldr tryAllKeybinds cFalse keybinds
+  where tryAllKeybinds bind x = liftA2 (.|.) x $ tryKeybind server key bind
+        cFalse = pure (CBool 0) :: IO CBool
+
+tryKeybind :: Server -> Key -> (Key, Action) -> IO CBool
+tryKeybind server pressedKey (matchKey, action) =
+  if pressedKey == matchKey then action server else pure 0
+
+keybinds :: [(Key, Action)]
+keybinds =
+  [ (xK_Escape, terminateDisplay)
+  , (xK_F1, cycleWindowNext)
+  ]
